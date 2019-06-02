e968d98cde2290785f873e4b120b5b8113ae34f7
Carl Fredrik Samson <cf@samson.no>
2019-06-02 22:44:23 +0200

    200 line version

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@
 #![feature(naked_functions)]
 use std::ptr;
 
-// In our simple example we set most constraints here.
 const DEFAULT_STACK_SIZE: usize = 1024 * 1024 * 8;
 const MAX_THREADS: usize = 4;
 static mut RUNTIME: usize = 0;
-static mut STACK_PTR: *mut usize = 0 as *mut usize;
 
 struct Runtime {
     threads: Vec<Thread>,
@@ -28,7 +26,7 @@ struct Thread {
 }
 
 #[derive(Debug, Default)]
-#[repr(C)] // not strictly needed but Rust ABI is not guaranteed to be stable
+#[repr(C)] 
 struct ThreadContext {
     rsp: u64,
     r15: u64,
@@ -41,9 +39,6 @@ struct ThreadContext {
 
 impl Thread {
     fn new(id: usize) -> Self {
-        // We initialize each thread here and allocate the stack. This is not neccesary,
-        // we can allocate memory for it later, but it keeps complexity down and lets us focus on more interesting parts
-        // to do it here. The important part is that once allocated it MUST NOT move in memory.
         Thread {
             id,
             stack: vec![0_u8; DEFAULT_STACK_SIZE],
@@ -55,7 +50,6 @@ impl Thread {
 
 impl Runtime {
     fn new() -> Self {
-        // This will be our base thread, which will be initialized in the `running` state
         let base_thread = Thread {
             id: 0,
             stack: vec![0_u8; DEFAULT_STACK_SIZE],
@@ -63,7 +57,6 @@ impl Runtime {
             state: State::Running,
         };
 
-        // We initialize the rest of our threads.
         let mut threads = vec![base_thread];
         let mut available_threads: Vec<Thread> = (1..MAX_THREADS).map(|i| Thread::new(i)).collect();
         threads.append(&mut available_threads);
@@ -74,8 +67,6 @@ impl Runtime {
         }
     }
 
-    /// This is cheating a bit, but we need a pointer to our Runtime stored so we can call yield on it even if
-    /// we don't have a reference to it.
     fn init(&self) {
         unsafe {
             let r_ptr: *const Runtime = self;
@@ -83,10 +74,6 @@ impl Runtime {
         }
     }
 
-    /// This is where we start running our runtime. If the current thread is not our base thread we set its state to
-    /// Parked. It means we're finished with it. Then we yield which will schedule a new thread to be run.
-    /// If it is our base thread, we call yield until it returns false (which means that there are no threads scheduled)
-    /// and we are done.
     fn run(&mut self) -> ! {
         let current = self.current;
         if current != 0 {
@@ -98,12 +85,6 @@ impl Runtime {
         std::process::exit(0);
     }
     
-    /// This is the heart of our runtime. Here we go through all threads and see if anyone is in the `Ready` state.
-    /// If no thread is `Ready` we're all done. This is an extremely simple sceduler using only a round-robin algorithm.
-    /// 
-    /// If we find a thread that's ready to be run we change the state of the current thread from `Running` to `Ready`.
-    /// Then we call swictch wich will save the current context (the old context) and load the new context
-    /// into the CPU which then resumes based on the context it was just passed.
     fn t_yield(&mut self) -> bool {
         let mut pos = self.current;
         while self.threads[pos].state != State::Ready {
@@ -132,23 +113,6 @@ impl Runtime {
         true
     }
 
-    /// While `yield` is the logically interesting function I think this the technically most interesting. 
-    /// 
-    /// 
-    /// When we spawn a new thread we first check if there are any available threads (threads in `Parked` state). 
-    /// If we run out of threads we panic in this scenario but there are several (better) ways to handle that. 
-    /// We keep things simple for now.
-    /// 
-    /// When we find an available thread we get the stack length and a pointer to our u8 bytearray.
-    /// 
-    /// The next part we have to use some unsafe functions. First we write an address to our `guard` function
-    /// that will be called if the function we provide returns. Then we set the address to the function we 
-    /// pass inn.
-    /// 
-    /// Third, we set the value of `rsp` which is the stack pointer to the address of our provided function so we start
-    /// executing that first when we are scheuled to run.
-    /// 
-    /// Lastly we set the state as `Ready` which means we have work to do and is ready to do it.
     fn spawn(&mut self, f: fn()) {
         let available = self
             .threads
@@ -164,13 +128,10 @@ impl Runtime {
             ptr::write(s_ptr.offset((size - 16) as isize) as *mut u64, f as u64);
             available.ctx.rsp = s_ptr.offset((size - 16) as isize) as u64;
         }
-
         available.state = State::Ready;
     }
 }
 
-/// This is our guard function that we place on top of the stack. All this function does is set the 
-/// state of our current thread and then `yield` which will then schedule a new thread to be run.
 fn guard() -> ! {
     unsafe {
         let rt_ptr = RUNTIME as *mut Runtime;
@@ -181,34 +142,12 @@ fn guard() -> ! {
     
 }
 
-/// We know that Runtime is alive the length of the program and that we only access from one core 
-/// (so no datarace). We yield execution of the current thread  by dereferencing a pointer to our 
-/// Runtime and then calling `t_yield` 
 fn yield_thread() {
     unsafe {
         let rt_ptr = RUNTIME as *mut Runtime;
         (*rt_ptr).t_yield();
     };
 }
-
-/// So here is our inline Assembly. As you remember from our first example this is just a bit more elaborate where we first
-/// read out the values of all the registers we need and then sets all the register values to the register values we 
-/// saved when we suspended exceution on the "new" thread.
-/// 
-/// This is essentially all we need to do to save and resume execution.
-/// 
-/// Some details about inline assembly:
-/// First ":" after ":" we have our output parameters, this is values we write data to. We use "=*m" since we pass a pointer
-/// in and we want to write to the location of the data the pointer points to.
-/// Second ":" we have the input parameters which is our "new" context. We only read from this data.
-/// Third ":" This our clobber list, this is information to the compiler that these registers can't be used freely
-/// Fourth ":" This is options we can pass inn, Rust has 3: "alignstack", "volatile" and "intel"
-/// For this to work (partially) on windows we need to use "alignstack" where the compiler adds the neccesary padding to
-/// make sure our stack is aligned.
-/// 
-/// One last important part (it will not work without this) is the #[naked] attribute. Basically this lets us have full
-/// control over the stack layout since normal functions has a prologue-and epilogue added by the
-/// compiler that will cause trouble for us. We avoid this by marking the funtion as "Naked".
 
 // see: https://github.com/rust-lang/rfcs/blob/master/text/1201-naked-fns.md
 #[naked]
@@ -230,7 +169,6 @@ unsafe fn switch(old: *mut ThreadContext, new: *const ThreadContext) {
         mov     0x28($1), %rbx
         mov     0x30($1), %rbp
         ret
-        
         "
     : "=*m"(old)
     : "r"(new)
@@ -242,7 +180,6 @@ unsafe fn switch(old: *mut ThreadContext, new: *const ThreadContext) {
 fn main() {
     let mut runtime = Runtime::new();
     runtime.init();
-
     runtime.spawn(|| {
         println!("THREAD 1 STARTING");
         let id = 1;
@@ -251,7 +188,6 @@ fn main() {
             yield_thread();
         }
     });
-
     runtime.spawn(|| {
         println!("THREAD 2 STARTING");
         let id = 2;
