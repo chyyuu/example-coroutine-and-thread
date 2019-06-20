17ebea39e4f55af7508a34fbe3c2fea02fe166d7
Carl Fredrik Samson <cf@samson.no>
2019-06-20 23:04:49 +0200

    decreased stack size to 1 mb

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![feature(naked_functions)]
 use std::ptr;
 
-const DEFAULT_STACK_SIZE: usize = 1024 * 1024 * 2;
+const DEFAULT_STACK_SIZE: usize = 1024 * 1024;
 const MAX_THREADS: usize = 4;
 static mut RUNTIME: usize = 0;
 
