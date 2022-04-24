1abacb338f86d50093e0325b5beae827116d381d
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2022-04-24 14:25:59 +0800

    add stackless coroutine

--- src/main.rs
+++ src/main.rs
@@ -1,201 +1,117 @@
-#![feature(naked_functions)]
-#![feature(asm)]
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Poll, Context};
+use std::task::{RawWaker, RawWakerVTable, Waker};
 
-use std::arch::asm;
-
-const DEFAULT_STACK_SIZE: usize = 1024 * 1024 * 2;
-const MAX_THREADS: usize = 4;
-static mut RUNTIME: usize = 0;
-
-pub struct Runtime {
-    threads: Vec<Thread>,
-    current: usize,
-}
-
-#[derive(PartialEq, Eq, Debug)]
 enum State {
-    Available,
+    Halted,
     Running,
-    Ready,
 }
 
-struct Thread {
-    id: usize,
-    stack: Vec<u8>,
-    ctx: ThreadContext,
+struct Fib {
     state: State,
 }
 
-#[derive(Debug, Default)]
-#[repr(C)]
-struct ThreadContext {
-    rsp: u64,
-    r15: u64,
-    r14: u64,
-    r13: u64,
-    r12: u64,
-    rbx: u64,
-    rbp: u64,
-}
-
-impl Thread {
-    fn new(id: usize) -> Self {
-        Thread {
-            id,
-            stack: vec![0_u8; DEFAULT_STACK_SIZE],
-            ctx: ThreadContext::default(),
-            state: State::Available,
-        }
+impl Fib {
+    fn waiter<'a>(&'a mut self) -> Waiter<'a> {
+        Waiter { fib: self }
     }
 }
 
-impl Runtime {
-    pub fn new() -> Self {
-        let base_thread = Thread {
-            id: 0,
-            stack: vec![0_u8; DEFAULT_STACK_SIZE],
-            ctx: ThreadContext::default(),
-            state: State::Running,
-        };
+struct Waiter<'a> {
+    fib: &'a mut Fib,
+}
 
-        let mut threads = vec![base_thread];
-        let mut available_threads: Vec<Thread> = (1..MAX_THREADS).map(|i| Thread::new(i)).collect();
-        threads.append(&mut available_threads);
+impl<'a> Future for Waiter<'a> {
+    type Output = ();
 
-        Runtime {
-            threads,
-            current: 0,
-        }
-    }
-
-    pub fn init(&self) {
-        unsafe {
-            let r_ptr: *const Runtime = self;
-            RUNTIME = r_ptr as usize;
-        }
-    }
-
-    pub fn run(&mut self) -> ! {
-        while self.t_yield() {}
-        std::process::exit(0);
-    }
-
-    fn t_return(&mut self) {
-        if self.current != 0 {
-            self.threads[self.current].state = State::Available;
-            self.t_yield();
-        }
-    }
-
-    #[inline(never)]
-    fn t_yield(&mut self) -> bool {
-        let mut pos = self.current;
-        while self.threads[pos].state != State::Ready {
-            pos += 1;
-            if pos == self.threads.len() {
-                pos = 0;
+    fn poll(mut self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
+        match self.fib.state {
+            State::Halted => {
+                self.fib.state = State::Running;
+                Poll::Ready(())
             }
-            if pos == self.current {
-                return false;
+            State::Running => {
+                self.fib.state = State::Halted;
+                Poll::Pending
             }
         }
-
-        if self.threads[self.current].state != State::Available {
-            self.threads[self.current].state = State::Ready;
-        }
-
-        self.threads[pos].state = State::Running;
-        let old_pos = self.current;
-        self.current = pos;
-
-        unsafe {
-            let old: *mut ThreadContext = &mut self.threads[old_pos].ctx;
-            let new: *const ThreadContext = &self.threads[pos].ctx;
-            asm!("call switch", in("rdi") old, in("rsi") new, clobber_abi("C"));
-        }
-        self.threads.len() > 0
-    }
-
-    pub fn spawn(&mut self, f: fn()) {
-        let available = self
-            .threads
-            .iter_mut()
-            .find(|t| t.state == State::Available)
-            .expect("no available thread.");
-
-        let size = available.stack.len();
-        unsafe {
-            let s_ptr = available.stack.as_mut_ptr().offset(size as isize);
-            let s_ptr = (s_ptr as usize & !15) as *mut u8;
-            std::ptr::write(s_ptr.offset(-16) as *mut u64, guard as u64);
-            std::ptr::write(s_ptr.offset(-24) as *mut u64, skip as u64);
-            std::ptr::write(s_ptr.offset(-32) as *mut u64, f as u64);
-            available.ctx.rsp = s_ptr.offset(-32) as u64;
-        }
-        available.state = State::Ready;
     }
 }
 
-#[naked]
-unsafe extern "C" fn skip() {
-    asm!("ret", options(noreturn))
+
+use std::collections::VecDeque;
+
+struct Executor {
+    fibs: VecDeque<Pin<Box<dyn Future<Output=()>>>>,
 }
 
-fn guard() {
-    unsafe {
-        let rt_ptr = RUNTIME as *mut Runtime;
-        (*rt_ptr).t_return();
-    };
+impl Executor {
+    fn new() -> Self {
+        Executor {
+            fibs: VecDeque::new(),
+        }
+    }
+
+    fn push<C, F>(&mut self, closure: C)
+    where
+        F: Future<Output=()> + 'static,
+        C: FnOnce(Fib) -> F,
+    {
+        let fib = Fib { state: State::Running };
+        self.fibs.push_back(Box::pin(closure(fib)));
+    }
+
+    fn run(&mut self) {
+        let waker = create();
+        let mut context = Context::from_waker(&waker);
+
+        while let Some(mut fib) = self.fibs.pop_front() {
+            match fib.as_mut().poll(&mut context) {
+                Poll::Pending => {
+                    self.fibs.push_back(fib);
+                },
+                Poll::Ready(()) => {},
+            }
+        }
+    }
 }
 
-pub fn yield_thread() {
-    unsafe {
-        let rt_ptr = RUNTIME as *mut Runtime;
-        (*rt_ptr).t_yield();
-    };
+
+
+
+pub fn create() -> Waker {
+    // Safety: The waker points to a vtable with functions that do nothing. Doing
+    // nothing is memory-safe.
+    unsafe { Waker::from_raw(RAW_WAKER) }
 }
 
-#[naked]
-#[no_mangle]
-unsafe extern "C" fn switch() {
-    asm!(
-        "mov [rdi + 0x00], rsp",
-        "mov [rdi + 0x08], r15",
-        "mov [rdi + 0x10], r14",
-        "mov [rdi + 0x18], r13",
-        "mov [rdi + 0x20], r12",
-        "mov [rdi + 0x28], rbx",
-        "mov [rdi + 0x30], rbp",
-        "mov rsp, [rsi + 0x00]",
-        "mov r15, [rsi + 0x08]",
-        "mov r14, [rsi + 0x10]",
-        "mov r13, [rsi + 0x18]",
-        "mov r12, [rsi + 0x20]",
-        "mov rbx, [rsi + 0x28]",
-        "mov rbp, [rsi + 0x30]",
-        "ret", options(noreturn)
-    );
-}
+const RAW_WAKER: RawWaker = RawWaker::new(std::ptr::null(), &VTABLE);
+const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+
+unsafe fn clone(_: *const ()) -> RawWaker { RAW_WAKER }
+unsafe fn wake(_: *const ()) { }
+unsafe fn wake_by_ref(_: *const ()) { }
+unsafe fn drop(_: *const ()) { }
+
+
+
 pub fn main() {
-    let mut runtime = Runtime::new();
-    runtime.init();
-    runtime.spawn(|| {
-        println!("THREAD 1 STARTING");
-        let id = 1;
-        for i in 0..10 {
-            println!("thread: {} counter: {}", id, i);
-            yield_thread();
-        }
-        println!("THREAD 1 FINISHED");
-    });
-    runtime.spawn(|| {
-        println!("THREAD 2 STARTING");
-        let id = 2;
-        for i in 0..15 {
-            println!("thread: {} counter: {}", id, i);
-            yield_thread();
-        }
-        println!("THREAD 2 FINISHED");
-    });
-    runtime.run();
+    let mut exec = Executor::new();
+
+    for instance in 1..=3 {
+        exec.push(move |mut fib| async move {
+            println!("{} A", instance);
+            fib.waiter().await;
+            println!("{} B", instance);
+            fib.waiter().await;
+            println!("{} C", instance);
+            fib.waiter().await;
+            println!("{} D", instance);
+        });
+    }
+
+    println!("Running");
+    exec.run();
+    println!("Done");
 }
